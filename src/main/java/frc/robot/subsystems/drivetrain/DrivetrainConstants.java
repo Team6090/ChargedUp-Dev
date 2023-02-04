9f9a0edee9a521973d849d258eb9ad41b65d7a64
AlexFirstRobotics <crossalex@waylandunion.org>
2023-02-04 13:14:49 -0500

    Basic Structure of ArmSystem

--- src/main/java/frc/robot/subsystems/drivetrain/DrivetrainConstants.java
+++ src/main/java/frc/robot/subsystems/drivetrain/DrivetrainConstants.java
@@ -16,6 +16,10 @@ public final class DrivetrainConstants {
 
   public static final int PCMModule = 58;
 
+  public static final int ArmRetractionMotorID = 21893; /* TODO Change out id */
+
+  public static final int ArmRetractionCANCoderID = 42823; /* TODO Change out id */
+
   // FIXME: update all CAN IDs
   // FIXME: update all steer offsets
   public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1;
