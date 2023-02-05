b3a5fa65453ed5a2f44d24e889a0fc04a2ac9f15
Zak <zakevans1@outlook.com>
2023-02-05 16:25:17 -0500

    Pivot Changes for Testing

--- src/main/java/frc/robot/subsystems/PivotSystem.java
+++ src/main/java/frc/robot/subsystems/PivotSystem.java
@@ -1,11 +1,13 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.sensors.CANCoder;
 
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.drivetrain.DrivetrainConstants;
 
@@ -18,16 +20,26 @@ public class PivotSystem extends SubsystemBase {
     TalonFX pivotMotorRight;
     TalonFX pivotMotorLeft;
 
+    private static final boolean DEBUGGING = true;
+
     /* Instantiate the Motors and CANCoder */
     public PivotSystem() {
     pivotMotorLeft = new TalonFX(DrivetrainConstants.PivotMotorLeftID);
     pivotMotorRight = new TalonFX(DrivetrainConstants.PivotMotorRightID);
     pivotAngleCanCoder = new CANCoder(DrivetrainConstants.PivotAngleCanCoderID);
     pivotSolenoidLock = new Solenoid(DrivetrainConstants.PCMModule, PneumaticsModuleType.CTREPCM, DrivetrainConstants.PivotLockSolenoid);
+    
+    if (DEBUGGING){
+        pivotMotorLeft.setNeutralMode(NeutralMode.Coast);
+        pivotMotorRight.setNeutralMode(NeutralMode.Coast); 
+    }else{
+        pivotMotorLeft.setNeutralMode(NeutralMode.Brake);
+        pivotMotorRight.setNeutralMode(NeutralMode.Brake);
+    }
     }
 
     /* Create code that moves the arm based on the power being applied */
-    public void PivotArm(double power){
+    public void PivotArm(double power) {
         pivotMotorLeft.set(TalonFXControlMode.Velocity, power);
         pivotMotorRight.set(TalonFXControlMode.Velocity, -power);
     }
@@ -37,13 +49,14 @@ public class PivotSystem extends SubsystemBase {
     }
 
     /* Retrive the cancoder's position to find the pivot of the arm */
-    public double GetArmPivot(){
+    public double GetArmPivot() {
         return pivotAngleCanCoder.getPosition();
     }
 
     @Override
     public void periodic() {
         /* If you have anything you want logged do it here */
+            SmartDashboard.putNumber("CanCoder Position", pivotAngleCanCoder.getPosition());
     }
 
 }
